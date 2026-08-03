bbc04ec9b1747ceccf1d312370e58f14ecb2d746
Shreyanshu Pandey <pandeyrishi562@gmail.com>
2026-08-04 04:51:47 +0530

    Update API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://sweet-shop-backend-au2m.onrender.com//api',
+  baseURL: 'https://sweet-shop-backend-au2m.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
