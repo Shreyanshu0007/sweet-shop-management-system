54587df6ea2323d3d03fd648ae15a71fb2d6f95f
Shreyanshu Pandey <pandeyrishi562@gmail.com>
2026-08-04 03:59:02 +0530

    fix Product imports

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Order = require('../models/Order');
 const Cart = require('../models/Cart');
-const Product = require('../models/Product');
+const Product = require('../models/product');
 
 // Create order from cart (checkout)
 const createOrder = async (req, res) => {
